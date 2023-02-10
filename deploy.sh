81e4da9feb77027936c241cc7fc39d688efa5271
许怀安 <keington@outlook.com>
2023-02-11 02:57:23 +0800

    使用GitHub pages

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+
+yarn dev
+
+# 进入生成的文件夹
+
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m '构建并发布到prod分支'
+
+git push -f git@github.com:keington/vuepress-vdoing-blog.git master:prod
+
+cd -
